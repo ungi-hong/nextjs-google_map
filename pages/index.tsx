51ff78e5c2cccdcf62c4dc26e2a3486aedf41403
uq-hong <80738142+uq-hong@users.noreply.github.com>
2021-12-22 14:57:09 +0900

    クリックで座標を移動するように実装した

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,37 @@
+import { useState } from "react";
 import GoogleMapReact from "google-map-react";
 
+interface OnCLickCoordinate {
+  lat: number;
+  lng: number;
+}
+
+interface SetLatLng {
+  x: number;
+  y: number;
+  lat: number;
+  lng: number;
+}
+
 export default function Create() {
+  const [onClickCoordinate, setOnClickCoordinate] = useState<OnCLickCoordinate>(
+    { lat: 35.7022589, lng: 139.7744733 }
+  );
+
   const defaultLatLng = {
     lat: 35.7022589,
     lng: 139.7744733,
   };
 
-  const setLatLng = ({ x, y, lat, lng, event }) => {
-    console.log(x, y, lat, lng, event);
-    console.log(lng);
+  const setLatLng = ({ x, y, lat, lng }: SetLatLng) => {
+    setOnClickCoordinate({ lat, lng });
   };
 
   return (
     <div style={{ height: "300px", width: "300px" }}>
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY }}
-        defaultCenter={defaultLatLng}
+        center={onClickCoordinate}
         defaultZoom={16}
         onClick={setLatLng}
       />
